11af668047b6f07b66932bc127d3224c9e12d9fb
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-25 12:20:10 -0800

    add all origins

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
