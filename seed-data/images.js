8a9caf5b2bd5f5d764b8149f5d99b0361448a633
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-26 16:52:11 -0800

    altered seed data

--- seed-data/images.js
+++ seed-data/images.js
@@ -2,16 +2,15 @@ const images = [
   {
     image_id: 1,
     title: "Ocean",
-    image: "https://res.cloudinary.com/dncmazfp0/image/upload/v1700679541/ytum5s1gxhwkvdwy4dua.jpg",
+    image: "https://res.cloudinary.com/hzjhihahh/image/upload/v1701045965/pplmqoeuq1zxscvhzeo0.jpg",
     category: "Underwater",
     likes: 5,
     user_id: 1,
   },
   {
     image_id: 2,
-    title: "Me",
-    image:
-      "https://res.cloudinary.com/dncmazfp0/image/upload/v1700696197/vdwvogsdrlh30gwmw5vq.webp",
+    title: "Gliding",
+    image: "https://res.cloudinary.com/hzjhihahh/image/upload/v1700976607/p39tbu3wt1h3hzflbahu.jpg",
     category: "Phone Selfie",
     likes: 5,
     user_id: 1,
@@ -19,7 +18,7 @@ const images = [
   {
     image_id: 3,
     title: "Captain",
-    image: "https://res.cloudinary.com/dncmazfp0/image/upload/v1700630284/trizzsukj5rrf4114ehp.jpg",
+    image: "https://res.cloudinary.com/hzjhihahh/image/upload/v1701045965/mx4kerlyr0cnph09afs5.jpg",
     category: "AI Selfie",
     likes: 5,
     user_id: 1,
@@ -27,7 +26,7 @@ const images = [
   {
     image_id: 4,
     title: "Framed",
-    image: "https://res.cloudinary.com/dncmazfp0/image/upload/v1700696259/zeeal7r4xmzm9bxxmd71.jpg",
+    image: "https://res.cloudinary.com/hzjhihahh/image/upload/v1701045965/cgsjin0zw74k2kjmrmo7.jpg",
     category: "Self Portrait",
     likes: 5,
     user_id: 1,
