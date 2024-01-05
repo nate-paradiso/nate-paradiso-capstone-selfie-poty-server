998f9210efd4189c39f04d9c89ea96aacd5442a6
Nate Paradiso <ipfcolo05@gmail.com>
2024-01-04 20:51:30 -0800

    working like button on server

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 const router = Router();
 import { getAllImages, updateLikes } from "../controllers/home-controller.js";
 
-router.route("").get(getAllImages).patch(updateLikes);
+router.route("").get(getAllImages);
+router.route("/likes").patch(updateLikes);
 
 export default router;
